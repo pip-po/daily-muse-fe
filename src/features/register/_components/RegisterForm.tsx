1e98e245ce3778ef50ebc5e74ea1feb06fc61a73
pip-po <officialomicumi@gmail.com>
2025-04-21 17:44:20 +0700

    Register (#1)
    * feat: add register
    
    * chore: add toast

---
+++ src/features/register/_components/RegisterForm.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import useRegister from "@/hooks/api/auth/useRegister";
+import { cn } from "@/lib/utils";
+import { useFormik } from "formik";
+import Link from "next/link";
+import { RegisterSchema } from "../schema";
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  const { mutateAsync: register, isPending } = useRegister();
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: RegisterSchema,
+    onSubmit: async (values) => {
+      await register(values);
+    },
+  });
+
+  // console.log(formik.errors);
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardDescription>
+            Enter your email below to create new account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={formik.handleSubmit}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  id="name"
+                  type="name"
+                  name="name"
+                  placeholder="your name"
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required
+                />
+                {!!formik.touched.name && !!formik.errors.name && (
+                  <p className="text-red-500">{formik.errors.name}</p>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="m@example.com"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required
+                />
+                {!!formik.touched.email && !!formik.errors.email && (
+                  <p className="text-red-500">{formik.errors.email}</p>
+                )}
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  placeholder="your password"
+                  value={formik.values.password}
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  required
+                />
+                {!!formik.touched.password && !!formik.errors.password && (
+                  <p className="text-red-500">{formik.errors.password}</p>
+                )}
+              </div>
+              <Button type="submit" className="w-full" disabled={isPending}>
+                {isPending ? "loading" : "register"}
+              </Button>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link href="#" className="underline underline-offset-4">
+                Register
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
